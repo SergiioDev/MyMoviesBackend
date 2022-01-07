49f0543797c59e9ef6f66d77855c7624c59f65d4
SergiioDev <sergi13456@hotmail.es>
2022-01-07 18:58:00 +0100

    register Movie model to admin and commented movie endpoints

---
+++ Movies/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from Movies import views
+
+urlpatterns = [
+    #path('movies', views.MoviesList.as_view()),
+#    path('create', views.MoviesCreate.as_view())
+]
