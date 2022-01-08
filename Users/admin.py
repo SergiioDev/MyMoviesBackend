cbf8cf6b9549c25edae7a502bbe8e37ced77af19
SergiioDev <sergi13456@hotmail.es>
2022-01-08 22:10:35 +0100

    Create User model and register to admin.py

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from Users.models import User
+
+
+class UserAdminConfig(UserAdmin):
+    model = User
+    search_fields = ('email', 'username', 'first_name',)
+    list_filter = ('email', 'username', 'first_name', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email', 'username', 'first_name',
+                    'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'first_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')
+        }),
+    )
+
+
+admin.site(User, UserAdminConfig)
