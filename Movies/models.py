61ef05e82a43ca742787823d02dfd1156fa07de7
SergiioDev <sergi13456@hotmail.es>
2022-01-13 12:06:08 +0100

    fix bug of genre field

--- Movies/models.py
+++ Movies/models.py
@@ -1,11 +1,10 @@
-from django.core.files.storage import FileSystemStorage
 from djongo import models
 from django.conf import settings
 from djongo.storage import GridFSStorage
-from django import forms
-from django.contrib.postgres.fields import ArrayField
 
 grid_fs_storage = GridFSStorage(collection='images', base_url=''.join([settings.BASE_URL, 'images/']))
+
+
 class Movie(models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=500)
@@ -14,15 +13,7 @@ class Movie(models.Model):
     release_date = models.DateField()
     average_rating = models.IntegerField(default=0)
     user_rating = models.IntegerField(default=0)
-    #genres = ArrayField(models.CharField(max_length=20))
+    genres = models.CharField(max_length=200)
     is_adult = models.BooleanField(default=False)
 
-
-#Note: Investigate if we need this
-#class MovieForm(forms.ModelForm):
-#   class Meta:
-#        model = Movie
-#        fields = (
-#            'movie_id', 'title', 'poster', 'header', 'release_date', 'average_rating', 'user_rating', 'genres',
-#            'is_adult'
-#        )
+    objects = models.DjongoManager()
